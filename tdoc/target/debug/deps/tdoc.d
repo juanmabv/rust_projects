2d1dff13a66d474cee2357a88acb01bf17d4c824
juanmabv <juanmabatistav@gmail.com>
2023-12-31 10:31:34 +0000

    created tdoc project

---
+++ tdoc/target/debug/deps/tdoc.d
@@ -0,0 +1,5 @@
+C:\Users\Gaming\rust_projects\tdoc\target\debug\deps\tdoc.exe: src\main.rs
+
+C:\Users\Gaming\rust_projects\tdoc\target\debug\deps\tdoc.d: src\main.rs
+
+src\main.rs:
---
+++ tdoc/target/debug/tdoc.d
@@ -0,0 +1,1 @@
+C:\Users\Gaming\rust_projects\tdoc\target\debug\tdoc.exe: C:\Users\Gaming\rust_projects\tdoc\src\main.rs
